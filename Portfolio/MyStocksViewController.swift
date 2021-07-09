6b69db81efa4c03b3790f49f1e39e0400abfd62f
Harshdeep Singh <harshdeepsingh3009@gmail.com>
2021-07-08 20:41:16 -0400

    Adding tableview to MyStocks Scene

---
+++ Portfolio/MyStocksViewController.swift
@@ -0,0 +1,50 @@
+//
+//  MyStocksViewController.swift
+//  Portfolio
+//
+//  Created by Harshdeep Singh on 7/8/21.
+//
+
+import UIKit
+
+class MyStocksViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet var tableView: UITableView!
+    
+    let myStocks = ["Amazon", "Tesla", "Google", "Facebook", "Sundial"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myStocks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = myStocks[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let cell = tableView.cellForRow(at: indexPath)
+        print("You tapped ",cell?.textLabel?.text! ?? "No data"," !!!!!")
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
